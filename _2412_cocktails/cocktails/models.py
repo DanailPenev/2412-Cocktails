45c36434a48990dd2179c6e8bd5b0ef62532a408
DanailPenev <fcbdelrio@gmail.com>
2017-03-07 17:07:20 +0000

    Initial database setup, probably needs some fixing, needs to be tested

--- _2412_cocktails/cocktails/models.py
+++ _2412_cocktails/cocktails/models.py
@@ -1,5 +1,43 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Cocktail(models.Model):
+	name = models.CharField(max_length=128, unique=False, null=False)
+	
+	def __str__(self):
+		return self.name
+	
+	def __unicode__(self):
+		return self.name
+
+
+class Ingredient(models.Model):
+	cocktails = models.ManyToManyField(Cocktail)
+	name = models.CharField(max_length=128, unique=True, null=False)
+	type = models.CharField(max_length=128, unique=False, null=False)
+	
+	def __str__(self):
+		return self.name
+		
+	def __unicode__(self):
+		return self.type
+		
+class Instruction(models.Model):
+	cocktails = models.ManyToManyField(Cocktail)
+	ingredients = models.ManyToManyField(Ingredient)
+	type = models.CharField(max_length=128, unique=False, null=False)
+	duration = models.IntegerField()
+	
+class UserProfile(models.Model):
+	#Links UserProfile to a User model instance
+	user = models.OneToOneField(User)
+	followee = models.ForeignKey('self', null=True)
+	
+	dob = models.DateField(auto_now=False, auto_now_add=False)
+	
+	def save(self, checkFollowee=True):
+		super(UserProfile, self).save()
+		if self.partner and checkFollowee:
+			self.followee.followee = self
+			self.followee.save(checkPartner=False)
