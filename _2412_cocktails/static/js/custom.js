49687bb758dead2a120ed1f96f44f96bc1a68910
DanailPenev <fcbdelrio@gmail.com>
2017-03-21 17:17:50 +0000

    Fixed registration functionality, started upload_cocktail

--- _2412_cocktails/static/js/custom.js
+++ _2412_cocktails/static/js/custom.js
@@ -2,3 +2,20 @@
 $('.carousel').carousel({
 	interval: 5000 //changes the speed
 })
+
+$(function() {
+    // this is on click event just to demo.
+    // You would probably run this at page load or quantity change.
+    $("#generate_forms").click(function() {
+		// update total form count
+		quantity = $("[name=quantity]").val();
+		$("[name=form-TOTAL_FORMS]").val(quantity);  
+
+		// copy the template and replace prefixes with the correct index
+		for (i=0;i<quantity;i++) {
+			// Note: Must use global replace here
+			html = $("#form_template").clone().html().replace(/__prefix_/g', i);
+			$("#forms").append(html);
+		};
+		})
+	})
