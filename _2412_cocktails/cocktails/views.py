96f4dc21bbac60108e0a34c892c7ad682f874446
Sridha Agarwal <2208414a@student.gla.ac.uk>
2017-03-22 14:54:37 +0000

    made changes to cocktails template

--- _2412_cocktails/cocktails/views.py
+++ _2412_cocktails/cocktails/views.py
@@ -120,7 +120,7 @@ def show_cocktail(request, cocktail_name_slug):
 	try:
 		cocktail = Cocktail.objects.get(slug=cocktail_name_slug)
 
-		ingredients = Ingredient.objects.filter(cocktail=cocktail)
+		ingredients = Ingredient.objects.filter(cocktail in cocktails)
 		instructions = Instruction.objects.filter(cocktail=cocktail)
 
 		context_dict['ingredients'] = ingredients
